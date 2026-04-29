8d839b318ef6117a72fed67fe35ca45c9fe1b15f
WilliamBazinet <wj18bazi@siena.edu>
2026-04-29 15:40:16 -0400

    CinemaScreen added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     // Firebase Authentication
     implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
     implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
